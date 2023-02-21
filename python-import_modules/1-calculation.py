792fac54b1f3ec1b355e4c5dd6fd77616e976fcb
Telesphore-Uwabera <t.uwabera@gmail.com>
2023-02-21 08:11:23 -0800

    K

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+# adding
 from calculator_1 import *
 from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-    a = 10
-    b = 5
-print("{} + {} = {}".format(a, b, add(a, b)))
-print("{} - {} = {}".format(a, b, sub(a, b)))
-print("{} / {} = {}".format(a, b, div(a, b)))
-print("{} * {} = {}".format(a, b, mul(a, b)))
+        a = 10
+        b = 5
+print("{} + {} = {}".format(a,b, add(a, b)))
+print("{} - {} = {}".format(a,b, sub(a, b)))
+print("{} * {} = {}".format(a,b, mul(a, b)))
+print("{} / {} = {}".format(a,b, div(a, b)))
