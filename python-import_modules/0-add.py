26a1e75b373b9026e41f0ba11df63a8bfac8ae8e
Telesphore-Uwabera <t.uwabera@gmail.com>
2023-02-21 01:21:54 -0800

    sa

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#Python program to add two numbers using function
+#aaa
+def add_num(a,b):#function for addition
+    sum=a+b;
+    return sum; #return value
+a=1 #variable declaration
+b=2
+print("The sum is",add_num(a,b))#call te function
