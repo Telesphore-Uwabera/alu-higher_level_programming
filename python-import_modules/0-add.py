9810c4dfd0d1a480aa12ae568639036aab0deb8c
Telesphore-Uwabera <t.uwabera@gmail.com>
2023-02-21 01:35:02 -0800

    GG

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 #Python program to add two numbers using function
-#aaa
 def add_num(a,b):#function for addition
     sum=a+b;
     return sum; #return value
-a=1 #variable declaration
-b=2
-print("The sum is",add_num(a,b))#call te function
+num1=1 #variable declaration
+num2=2
+print("The sum is",add_num(num1,num2))#call te function
