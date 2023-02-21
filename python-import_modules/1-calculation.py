b6e43e66b988b3b82f9f99482fd9a9bdd7ccf495
Telesphore-Uwabera <t.uwabera@gmail.com>
2023-02-21 07:42:22 -0800

    J

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-from calculator_1 import calculator
+from calculator_1 import *
 if __name__ == "__main__":
     a = 10
     b = 5
 print("{} + {} = {}".format(a, b, add(a, b)))
-print("{} - {} = {}".format(a, b, subtract(a, b)))
-print("{} / {} = {}".format(a, b, divide(a, b)))
-print("{} * {} = {}".format(a, b, multiply(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
