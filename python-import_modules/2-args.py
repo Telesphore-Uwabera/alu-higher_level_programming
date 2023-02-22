d70b6622ff97e6af1767cc2cfce487d775895264
Telesphore-Uwabera <t.uwabera@alustudent.com>
2023-02-22 00:45:01 -0800

    K

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def no_of_argu(*args):
+
+	# using len() method in args to count
+	return(len(args))
+
+print(no_of_argu(2, 5, 4))
+print(no_of_argu(4, 5, 6, 5, 4, 4))
+print(no_of_argu(3, 2, 32, 4, 4, 52, 1))
+print(no_of_argu(1))
+
