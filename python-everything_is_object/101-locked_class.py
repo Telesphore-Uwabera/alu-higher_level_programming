7d99e471a970c2cd7862742a569cbbd8f2c2cb0e
Telesphore-Uwabera <t.uwabera@alustudent.com>
2023-03-20 08:42:26 -0700

    alias

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+def __setattr__(self, attribute, ):
     if not attribute in self.__dict__:
-        print "Cannot set %s" %attribute
+        print "Cannot set %s" % attribute
     else:
         self.__dict__[attribute] = first_name
