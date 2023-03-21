efed9a2617fe085ec709e8d65542e2b8724c7ea3
Telesphore-Uwabera <t.uwabera@alustudent.com>
2023-03-21 01:51:11 -0700

    alias

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def magic_string():
-    for i in range(10):
-        return f"BestSchool".split(",")
+def magic_string(my_list=[]):
+    my_list.append("BestSchool")
+    return (', '.join(my_list))
+    
