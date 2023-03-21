647888db519762cc2c36e1855ec88aaa83a9f040
Telesphore-Uwabera <t.uwabera@alustudent.com>
2023-03-21 02:22:37 -0700

    a

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -3,4 +3,4 @@ def lookup(obj):
     """
     Return a list of attributes and methods for the given object.
     """
-    return [attr for attr in dir(obj) if not attr.startswith("__")]
+    return dir(obj)
