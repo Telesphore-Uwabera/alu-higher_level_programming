2ed87fdafa06c847efafb8669cc68a9e32defab1
Telesphore-Uwabera <t.uwabera@alustudent.com>
2023-03-23 05:02:05 -0700

    abcd

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+''' Checking if the object belongs to a class'''
+
+
 def is_same_class(obj, a_class):
-    """Return True if obj is exactly an instance of the specified class; otherwise False."""
+    ''' Checking if the object belongs to a class'''
     return type(obj) is a_class
