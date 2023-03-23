6b894d5764bea7aad0c469fbb58fe703b0b6f7bd
Telesphore-Uwabera <t.uwabera@alustudent.com>
2023-03-23 05:48:20 -0700

    asa

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    for i in range(10):
-        return f"BestSchool".split(",i")
+    
+        return f"BestSchool".split(",")
--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,12 +1,1 @@
 #!/usr/bin/python3
-class LockedClass:
-    __slots__ = ("first_name",)
-    print(Lockedclass.__doc__)
-
-    def __init__(self):
-        self.first_name = None
-
-    def __setattr__(self, name, value):
-        if not hasattr(self, "first_name") and name != "first_name":
-            raise AttributeError("LockedClass does not allow creating new instance attributes")
-        super().__setattr__(name, value)
