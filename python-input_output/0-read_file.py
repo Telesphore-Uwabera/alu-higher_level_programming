9f7fca99a2ed2049dd1bd241d9e9ce837c923309
Telesphore-Uwabera <t.uwabera@alustudent.com>
2023-03-23 22:30:33 -0700

    abana

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""Reading a text file"""
+
+
 def read_file(filename=""):
-    with open(filename, "r", encoding="utf8") as file:
-        content = file.read()
-    return content
+    """A functions that reads a text file and prints it to stdout
+    Args:
+        filename (str): The name of the file to read from.
+    """
 
-
-print(read_file)
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
