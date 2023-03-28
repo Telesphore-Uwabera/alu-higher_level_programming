f0b0272865c1bfc113ae7fdc33d9ad604e22f7e7
Telesphore-Uwabera <t.uwabera@alustudent.com>
2023-03-27 20:11:26 -0700

    asap

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,14 +1,2 @@
-#Starting MySQL database server mysqld 
-#!/bin/bash
-
-# Enter MySQL credentials
-MYSQL_USER="your_username"
-MYSQL_PASSWORD="your_password"
-
-# Get a list of all databases
-DATABASES=$(mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema)")
-
-# Loop through the list and output each database name
-for DB in $DATABASES; do
-  echo "- $DB"
-done
+--show databases
+SHOW DATABASES
