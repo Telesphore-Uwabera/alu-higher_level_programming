7f1445bf3edb7717d97a301a66486f99b4e61d52
Telesphore-Uwabera <t.uwabera@alustudent.com>
2023-03-28 13:14:46 -0700

    h

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
---  script that inserts a new row
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
+-- insert into
+INSERT INTO first_table(id, name) VALUES(89, 'Holberton School');
