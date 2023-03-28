86ad901a6bc8b8e01476ccc13f8d75194e32a13c
Telesphore-Uwabera <t.uwabera@alustudent.com>
2023-03-28 13:13:11 -0700

    h

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,5 @@
--- all records with a score >= 10
-SELECT `score`, `name` FROM `second_table` WHERE `score` >= 10 ORDER BY `score` DESC;
+-- Lists all records of the table second_table.
+-- Records are ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
