dedaa6d034f7d7569565d97b3574ccc33c030b8d
Telesphore-Uwabera <t.uwabera@alustudent.com>
2023-03-28 13:11:42 -0700

    h

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,6 @@
---  lists all records of the table, Don’t list rows without a name value
-SELECT `name`, `score` FROM `second_table` WHERE `name` != "" ORDER BY `score` DESC;
+-- Lists all records of the table second_table having a name value in my MySQL server.
+-- Records are ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `name` != ""
+ORDER BY `score` DESC
