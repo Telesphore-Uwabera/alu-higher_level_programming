c755008e235c82bfd1d9bebf343eba9a0e99017e
Telesphore-Uwabera <t.uwabera@alustudent.com>
2023-03-29 02:45:23 -0700

    a

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 # script that adds all arguments to a Python list,
 # and then save them to a file
-"""
-    script that adds all arguments to a Python list,
-    and save them to a file
-"""
+
+
+
 
 import sys
 
