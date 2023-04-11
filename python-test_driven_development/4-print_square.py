a44e2052313cb1b9e181c3cdeedc544470b1fb93
root <t.uwabera@alustudent.com>
2023-04-11 06:09:56 -0700

    g

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+""""Doc"""
+
+
 def print_square(size):
-    if not isinstance(size, int):
+    """"Doc"""
+    if not isinstance(size, (int,)):
         raise TypeError("size must be an integer")
+
     if size < 0:
         raise ValueError("size must be >= 0")
-    for i in range(size):
+
+    for _ in range(size):
         print("#" * size)
