dde329678f0c1bf8c77a9c2c94227e9d24f2b8ba
root <t.uwabera@alustudent.com>
2023-04-12 11:19:21 -0700

    s

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""module documentation"""
+"""Module Documentation"""
 
 
 def say_my_name(first_name, last_name=""):
-    """"function documentation"""
-    if not isinstance(first_name, str):
+    """"Function Documentation"""
+    if not isinstance(first_name, (str,)):
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+    if not isinstance(last_name, (str,)):
         raise TypeError("last_name must be a string")
-    full_name = first_name + " " + last_name
-    if last_name:
-        print("My name is", full_name)
-    else:
-        print("My name is", first_name)
+
+    print("My name is {} {}".format(first_name, last_name))
