4eef625e5e22d9f8224812bd8a666286675103c1
root <t.uwabera@alustudent.com>
2023-04-17 03:29:55 -0700

    h

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Lists all cities of a given state from the database hbtn_0e_4_usa"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to MySQL database
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=username, passwd=password, db=db_name)
+
+    # Create cursor to execute queries
+    cursor = db.cursor()
+
+    # Execute the query to get cities of a given state
+    cursor.execute("SELECT cities.id, cities.name, states.name \
+                    FROM cities JOIN states ON cities.state_id = states.id \
+                    WHERE states.name = %s \
+                    ORDER BY cities.id ASC", (state_name,))
+
+    # Fetch all the rows as a list of tuples
+    rows = cursor.fetchall()
+
+    # Print the results
+    for row in rows:
+        print(row)
+
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
