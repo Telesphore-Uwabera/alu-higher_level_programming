9aaaf264f230e9ee3fa11afd90f880eb0e09925f
root <t.uwabera@alustudent.com>
2023-04-17 04:08:17 -0700

    h

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""module documentation"""
+#!/usr/bin/env python3
+"""Class definition of a State and an instance Base = declarative_base()"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+# Create a declarative base instance
+Base = declarative_base()
+
+
+# Define the State class
+class State(Base):
+    """State class that links to the MySQL table states"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
