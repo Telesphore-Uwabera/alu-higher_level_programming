1332c3c46f26b4be2e67ce35f3d1de48f13855ff
root <t.uwabera@alustudent.com>
2023-04-17 02:28:38 -0700

    h

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -6,7 +6,7 @@ import sys
 
 
 if __name__ == '__main__':
-    # Get MySQL username, password and database name from command-line arguments
+    # Get MySQL username, password and database name
     mysql_username, mysql_password, database_name = sys.argv[1:]
 
     # Connect to MySQL server on localhost at port 3306
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     # Create a cursor object to execute SQL queries
     cursor = conn.cursor()
 
-    # Execute SQL query to retrieve all states with a name starting with N (upper N)
+    # Execute SQL query to retrieve all states with a name starting with N
     cursor.execute(
         "SELECT * FROM states "
         "WHERE BINARY name LIKE 'N%' "
