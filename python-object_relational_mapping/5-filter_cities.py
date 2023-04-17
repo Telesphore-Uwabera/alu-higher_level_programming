28aed1d683cdd9001386d11f3afb9ecbdd3743fe
root <t.uwabera@alustudent.com>
2023-04-17 03:40:54 -0700

    h

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,34 +1,38 @@
 #!/usr/bin/python3
 """Lists all cities of a given state from the database hbtn_0e_4_usa"""
 
+#!/usr/bin/python3
+"""Lists all cities of a given state from the database hbtn_0e_4_usa"""
+
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    # Get the MySQL username, password, database name, and state name as arguments
-    username, password, database, state_name = sys.argv[1:]
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
 
-    # Establish a connection to the MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    # Connect to MySQL database
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=username, passwd=password, db=db_name)
 
-    # Create a cursor object to execute SQL queries
-    cur = db.cursor()
+    # Create cursor to execute queries
+    cursor = db.cursor()
 
-    # Execute a SELECT statement to retrieve all cities of the specified state, ordered by id
-    query = """
-        SELECT cities.id, cities.name, states.name
-        FROM cities
-        INNER JOIN states ON cities.state_id = states.id
-        WHERE states.name = %s
-        ORDER BY cities.id ASC
-    """
-    cur.execute(query, (state_name,))
+    # Execute the query to get cities of a given state
+    cursor.execute("SELECT cities.id, cities.name, states.name \
+                    FROM cities JOIN states ON cities.state_id = states.id \
+                    WHERE states.name = %s \
+                    ORDER BY cities.id ASC", (state_name,))
 
-    # Retrieve all the rows returned by the query and print them to the console
-    cities = cur.fetchall()
+    # Fetch all the rows as a list of tuples
+    rows = cursor.fetchall()
+
+    # Print the results
     for city in cities:
-        print(city)
+        print(", ".join(city[0] for city in cursor.fetchall()))
 
-    # Close the cursor and database connections to release resources
-    cur.close()
+    # Close cursor and database connection
+    cursor.close()
     db.close()
