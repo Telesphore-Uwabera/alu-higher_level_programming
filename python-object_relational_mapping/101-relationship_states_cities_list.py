f63a99672acfade6bd454b199fad65fe571fc077
TelesphoreUwabera <t.uwabera@alustudent.com>
2023-04-18 09:00:25 +0200

    advanced

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,42 +1,28 @@
 #!/usr/bin/python3
-"""module is documentede"""
+"""
+Lists all State objects and corresponding City objects
+"""
+
 
 import sys
+from relationship_state import Base, State
+from relationship_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_state import State
-from relationship_city import Base, City
 
-if __name__ == "__main__":
-    # Check for correct usage
-    if len(sys.argv) != 4:
-        print("Usage: ./101-relationship_states_cities_list.py <mysql username> <mysql password> <database name>")
-        exit()
 
-    # Get user credentials and database name from command line arguments
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    db_name = sys.argv[3]
-
-    # Set up connection to database
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(user, passwd, db_name), pool_pre_ping=True)
-
-    # Create table structure
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query database for all states and their corresponding cities, sorted by state and city IDs
-    states = session.query(State).order_by(State.id, City.id).all()
+    st = session.query(State).outerjoin(City).order_by(State.id, City.id).all()
 
-    # Print results
-    for state in states:
+    for state in st:
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
             print("    {}: {}".format(city.id, city.name))
-
-    # Close the session
-    session.close()
