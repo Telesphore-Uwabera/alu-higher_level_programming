228d98e5100900a3aebf3a3702ef600c35725c51
TelesphoreUwabera <t.uwabera@alustudent.com>
2023-06-05 17:24:06 +0200

    Update 12-object.js

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,33 +1,9 @@
 #!/usr/bin/node
 
-function findSecondBiggest(args) {
-  if (args.length <= 1) {
-    return 0;
-  }
+const obj = { value: 12 };
 
-  let biggest = Number.MIN_SAFE_INTEGER;
-  let secondBiggest = Number.MIN_SAFE_INTEGER;
+console.log(obj);
 
-  for (let i = 0; i < args.length; i++) {
-    const num = parseInt(args[i]);
-    if (!isNaN(num)) {
-      if (num === 12) {
-        args[i] = 89;
-      }
+obj.value = 89;
 
-      if (num > biggest) {
-        secondBiggest = biggest;
-        biggest = num;
-      } else if (num > secondBiggest && num < biggest) {
-        secondBiggest = num;
-      }
-    }
-  }
-
-  return secondBiggest;
-}
-
-const args = process.argv.slice(2);
-const result = findSecondBiggest(args);
-console.log(result);
-
+console.log(obj);
