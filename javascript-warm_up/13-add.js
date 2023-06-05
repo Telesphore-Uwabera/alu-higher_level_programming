5cf51570fe9535f89ec6ff2bb3b9c7e71b5e6179
TelesphoreUwabera <t.uwabera@alustudent.com>
2023-06-05 17:34:08 +0200

    Update 13-add.js

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
 
-function add(a, b) {
+exports.add = function(a, b) {
   return a + b;
-}
+};
+const { add } = require('./13-add');
 
 console.log(add(4, 85));
+console.log(add(93, -4));
+console.log(add(0, 89));
