ac15602244ac92f1d3b042127a4495cbb6a13325
TelesphoreUwabera <t.uwabera@alustudent.com>
2023-06-05 17:16:39 +0200

    Update 8-square.js

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -6,8 +6,10 @@ if (isNaN(size)) {
   console.log('Missing size');
 } else {
   let square = '';
+
   for (let i = 0; i < size; i++) {
     square += 'X'.repeat(size) + '\n';
   }
+
   console.log(square);
 }
