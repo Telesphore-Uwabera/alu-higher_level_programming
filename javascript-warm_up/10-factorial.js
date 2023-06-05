02008501ef3d06be8c72b45e1c96c0cc0c22bdeb
TelesphoreUwabera <t.uwabera@alustudent.com>
2023-06-05 17:01:37 +0200

    Update 10-factorial.js

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,17 +1,11 @@
 #!/usr/bin/node
 
 function factorial(n) {
-  if (isNaN(n)) {
+  if (isNaN(n) || n < 0) {
     return 1;
   }
-
-  if (n === 0 || n === 1) {
-    return 1;
-  } else {
-    return n * factorial(n - 1);
-  }
+  return (n === 0) ? 1 : n * factorial(n - 1);
 }
 
 const arg = parseInt(process.argv[2]);
-const result = factorial(arg);
-console.log(result);
+console.log(factorial(arg));
