63e5543cddd9c7b0a955d1e35bbc3eb61fe563b1
TelesphoreUwabera <t.uwabera@alustudent.com>
2023-06-08 18:57:11 +0200

    Update 100-let_me_const.js

--- javascript-warm_up/100-let_me_const.js
+++ javascript-warm_up/100-let_me_const.js
@@ -2,10 +2,9 @@
 // Contents of script.js file
 
 // Define the initial value of myVar
-var myVar = 40;
 
 // Modify the value of myVar to 333
 myVar = 333;
 
 // Print the updated value of myVar to the console
-//console.log(myVar);
+console.log(myVar);
