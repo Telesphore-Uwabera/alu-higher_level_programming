907f4320a0e9f41805aedddf2b4e8bb45c7afd4a
TelesphoreUwabera <t.uwabera@alustudent.com>
2023-06-08 20:09:35 +0200

    Update 103-object_fct.js

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -4,11 +4,13 @@ const myObject = {
   value: 12
 };
 
+
+console.log(myObject);
+
 myObject.incr = function () {
   this.value++;
 };
 
-console.log(myObject);
 myObject.incr();
 console.log(myObject);
 myObject.incr();
