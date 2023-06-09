db8dbebce4637bb392de4a9be90d6092609f75ae
TelesphoreUwabera <t.uwabera@alustudent.com>
2023-06-09 11:42:21 +0200

    Update 102-concat.js

--- javascript_objects_scopes_closures/102-concat.js
+++ javascript_objects_scopes_closures/102-concat.js
@@ -1,17 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-// Read the paths of the source files and the destination file from command-line arguments
-const [, , fileA, fileB, fileC] = process.argv;
-
-// Read the contents of the source files
-const content1 = fs.readFileSync(fileA, 'utf8');
-const content2 = fs.readFileSync(fileB, 'utf8');
-
-// Concatenate the contents of the source files
-const concatenatedContent = content1 + content2;
-
-// Write the concatenated content to the destination file
-fs.writeFileSync(fileC, concatenatedContent);
-
-console.log(`Files ${fileA} and ${fileB} have been concatenated to ${fileC}.`);
+const a = fs.readFileSync(process.argv[2], 'utf8');
+const b = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], a + b);
