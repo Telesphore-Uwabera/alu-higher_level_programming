12fd58b8bc1e00be27290ae8cdedc9e47a886939
TelesphoreUwabera <t.uwabera@alustudent.com>
2023-06-09 09:23:15 +0200

    Update 2-rectangle.js

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 class Rectangle {
   constructor(w, h) {
-    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-      return {}; // Return an empty object if w or h is invalid
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return {}; // Return an empty object
     }
+
     this.width = w;
     this.height = h;
   }
 }
-
-module.exports = Rectangle;
