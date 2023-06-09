949e0ad4ffd03a2e6bb5403267817e26821f02bf
TelesphoreUwabera <t.uwabera@alustudent.com>
2023-06-09 07:04:56 +0200

    Update 2-rectangle.js

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,8 +2,7 @@
 class Rectangle {
   constructor(w, h) {
     if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-      // Create an empty object if width or height is 0 or not a positive integer
-      return {};
+      return {}; // Create an empty object if width or height is 0 or not a positive integer
     }
 
     this.width = w;
