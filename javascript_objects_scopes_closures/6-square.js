2105dec88fc995cbbc1c83b4025bac846ae050f6
TelesphoreUwabera <t.uwabera@alustudent.com>
2023-06-09 12:39:51 +0200

    Update 6-square.js

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
-const Square = require ('./5-square');
+const Square = require('./5-square');
 
 class SquareWithCharPrint extends Square {
-  charPrint(c) {
+  charPrint (c) {
     if (c === undefined) {
       c = 'X';
     }
 
     for (let i = 0; i < this.height; i++) {
-      console.log (c.repeat(this.width));
+      console.log(c.repeat(this.width));
     }
   }
 }
