992115764d87eff40c5cd115a9603982fe4f0481
TelesphoreUwabera <t.uwabera@alustudent.com>
2023-06-09 07:38:14 +0200

    Update 3-rectangle.js

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -9,7 +9,7 @@ class Rectangle {
   }
 
   print() {
-    for (let i = 0; i < this.height; i++)  {
+    for (let i = 0; i < this.height; i++) {
       let row = '';
       for (let j = 0; j < this.width; j++) {
         row += 'X';
