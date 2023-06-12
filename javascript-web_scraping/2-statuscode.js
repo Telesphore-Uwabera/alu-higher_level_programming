d706f52d2622260944f6502bd2b8f18e21f7f638
Telesphore-Uwabera <t.uwabera@alustudent.com>
2023-06-12 13:08:41 +0200

    :

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,8 +3,7 @@
 const request = require('request');
 
 if (process.argv.length > 2) {
-  const url = process.argv[2];
-  
+  const url = process.argv[2];  
   request.get(url, (error, response) => {
     if (error) {
       console.error(`An error occurred while making the request: ${error}`);
