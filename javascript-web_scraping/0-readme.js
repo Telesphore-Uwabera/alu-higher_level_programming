833fabf17599aae0d625d3a63abe35ffb1d652f3
Telesphore-Uwabera <t.uwabera@alustudent.com>
2023-06-12 12:48:04 +0200

    K

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
-const fs = require('fs')
-fs.readFile('tp.txt', (err, inputD) => {
-   if (err) throw err;
-      console.log(inputD.toString());
-})
+const fs = require('fs');
+
+function readFile(filePath) {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      console.error(`An error occurred while reading the file: ${err}`);
+    } else {
+      console.log(data);
+    }
+  });
+}
